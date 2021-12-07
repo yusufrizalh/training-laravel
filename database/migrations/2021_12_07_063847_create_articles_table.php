ca82352d4635c6b694b24580d992fa6395a70693
Yusuf Rizal <yusufrizalh@gmail.com>
2021-12-07 13:58:15 +0700

    database migrations

---
+++ database/migrations/2021_12_07_063847_create_articles_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('body');
+            $table->timestamp('edited_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
